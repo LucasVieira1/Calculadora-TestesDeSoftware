e313ccd34ba13ab23a90437e9551662ee522eda0
LucasVieira1 <vieiraalucas1@gmail.com>
2022-08-26 21:38:50 -0300

    Fazendo testes e comentando o codigo

--- src/Testes.java
+++ src/Testes.java
@@ -3,60 +3,71 @@ public class Testes {
 	public static void main(String[] args) {
 
 		// SOMA
-		Soma som = new Soma(); //instanciando a classe Soma com nome "som"
-		int soma = som.somar(3, 7); //criando um atributo do tipo int, chamando o metodo somar com os valores declarados
-		System.out.println(soma); //printando o atibuto "soma" no console
+		Soma som = new Soma(); // instanciando a classe Soma com nome "som"
+		int soma = som.somar(3, 7); // criando um atributo do tipo "int", chamando o metodo somar com os valores
+									// declarados
+		System.out.println(soma); // printando o atibuto "soma" no console, vai retornar "10"
 
-		soma = som.somar(3, 0); //
-		System.out.println(soma);
+		soma = som.somar(3, 0);
+		System.out.println(soma); // vai retornar "3"
 
 		soma = som.somar(0, 0);
-		System.out.println(soma);
+		System.out.println(soma); // vai retornar "0"
 
 		soma = som.somar(3, -1);
-		System.out.println(soma);
+		System.out.println(soma); // vai retornar "2"
 
 		// SUBTRAÇÃO
-		Subtracao sub = new Subtracao(); //instanciando a classe Subtração com nome "sub"
-		int subtracao = sub.subtrair(3, 7); //criando um atributo do tipo int, chamando o metodo subtrair com os valores declarados
-		System.out.println(subtracao); //printando o atibuto "subtracao" no console
+		Subtracao sub = new Subtracao(); // instanciando a classe Subtração com nome "sub"
+		int subtracao = sub.subtrair(3, 7); // criando um atributo do tipo "int", chamando o metodo subtrair com os
+											// valores declarados
+		System.out.println(subtracao); // printando o atibuto "subtracao" no console, vai retornar "-4"
 
-		subtracao = sub.subtrair(3, 0);
+		subtracao = sub.subtrair(3, 0); // vai retornar "3"
 		System.out.println(subtracao);
 
-		subtracao = sub.subtrair(0, 0);
+		subtracao = sub.subtrair(0, 0); // vai retornar "0"
 		System.out.println(subtracao);
 
-		subtracao = sub.subtrair(3, -1);
+		subtracao = sub.subtrair(3, -1); // vai retornar "4"
 		System.out.println(subtracao);
 
 		// MULTIPLICAÇÃO
-		Multiplicacao mult = new Multiplicacao(); //instanciando a classe Multiplicação com nome "mult"
-		int multiplicacao = mult.multiplicar(3, 7); //criando um atributo do tipo int, chamando o metodo multiplicar com os valores declarados
-		System.out.println(multiplicacao); //printando o atibuto "multiplicacao" no console
+		Multiplicacao mult = new Multiplicacao(); // instanciando a classe Multiplicação com nome "mult"
+		int multiplicacao = mult.multiplicar(3, 7); // criando um atributo do tipo "int", chamando o metodo multiplicar
+													// com os valores declarados
+		System.out.println(multiplicacao); // printando o atibuto "multiplicacao" no console, vai retornar "21"
 
-		multiplicacao = mult.multiplicar(3, 0);
+		multiplicacao = mult.multiplicar(3, 0); // vai retornar "0"
 		System.out.println(multiplicacao);
 
-		multiplicacao = mult.multiplicar(0, 0);
+		multiplicacao = mult.multiplicar(0, 0); // vai retornar "0"
 		System.out.println(multiplicacao);
 
-		multiplicacao = mult.multiplicar(3, -1);
+		multiplicacao = mult.multiplicar(3, -1); // vai retornar "-3"
 		System.out.println(multiplicacao);
 
 		// DIVISÃO
-		Divisao div = new Divisao(); //instanciando a classe Divisão com nome "div"
-		int divisao = div.dividir(3, 7); //criando um atributo do tipo int, chamando o metodo dividir com os valores declarados
-		System.out.println(divisao); //printando o atibuto "Divisão" no console, 
+		Divisao div = new Divisao(); // instanciando a classe Divisão com nome "div"
+		int divisao = div.dividir(3, 7); // criando um atributo do tipo "int", chamando o metodo dividir com os valores
+											// declarados
+		System.out.println(divisao); // printando o atibuto "Divisão" no console, vai retornar um erro, pois o
+										// atributo é do tipo "int", o resultado vai ser um "float", a solução seria
+										// trocar
+										// os atributos do metodo "dividir" e do atributo "divisão" para float, o tipo
+										// "int"
+										// não é recomendado para esse tipo de cálculo.
 
 		divisao = div.dividir(3, 0);
-		System.out.println(divisao);
+		System.out.println(divisao); // printando o atibuto "Divisão" no console, vai retornar um erro, pois o numero
+										// "3" não é divisível por "0", não é possivel dividir um número inteiro por "0"
 
 		divisao = div.dividir(0, 0);
-		System.out.println(divisao);
+		System.out.println(divisao); // printando o atibuto "Divisão" no console, vai retornar um erro, pois "0"
+										// dividido pra "0" é igual a um resultado indefinido
 
 		divisao = div.dividir(3, -1);
-		System.out.println(divisao);
+		System.out.println(divisao); // printando o atibuto "Divisão" no console, vai retornar "-3"
 
 	}
 }
