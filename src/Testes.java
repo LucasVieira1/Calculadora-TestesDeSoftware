6b20190b3d80297441352be7aac78cd123386f29
LucasVieira1 <vieiraalucas1@gmail.com>
2022-08-26 21:37:27 -0300

    Fazendo testes e comentando o codigo

--- src/Testes.java
+++ src/Testes.java
@@ -3,11 +3,11 @@ public class Testes {
 	public static void main(String[] args) {
 
 		// SOMA
-		Soma som = new Soma();
-		int soma = som.somar(3, 7);
-		System.out.println(soma);
+		Soma som = new Soma(); //instanciando a classe Soma com nome "som"
+		int soma = som.somar(3, 7); //criando um atributo do tipo int, chamando o metodo somar com os valores declarados
+		System.out.println(soma); //printando o atibuto "soma" no console
 
-		soma = som.somar(3, 0);
+		soma = som.somar(3, 0); //
 		System.out.println(soma);
 
 		soma = som.somar(0, 0);
@@ -17,9 +17,9 @@ public class Testes {
 		System.out.println(soma);
 
 		// SUBTRAÇÃO
-		Subtracao sub = new Subtracao();
-		int subtracao = sub.subtrair(3, 7);
-		System.out.println(subtracao);
+		Subtracao sub = new Subtracao(); //instanciando a classe Subtração com nome "sub"
+		int subtracao = sub.subtrair(3, 7); //criando um atributo do tipo int, chamando o metodo subtrair com os valores declarados
+		System.out.println(subtracao); //printando o atibuto "subtracao" no console
 
 		subtracao = sub.subtrair(3, 0);
 		System.out.println(subtracao);
@@ -31,9 +31,9 @@ public class Testes {
 		System.out.println(subtracao);
 
 		// MULTIPLICAÇÃO
-		Multiplicacao mult = new Multiplicacao();
-		int multiplicacao = mult.multiplicar(3, 7);
-		System.out.println(multiplicacao);
+		Multiplicacao mult = new Multiplicacao(); //instanciando a classe Multiplicação com nome "mult"
+		int multiplicacao = mult.multiplicar(3, 7); //criando um atributo do tipo int, chamando o metodo multiplicar com os valores declarados
+		System.out.println(multiplicacao); //printando o atibuto "multiplicacao" no console
 
 		multiplicacao = mult.multiplicar(3, 0);
 		System.out.println(multiplicacao);
@@ -44,11 +44,11 @@ public class Testes {
 		multiplicacao = mult.multiplicar(3, -1);
 		System.out.println(multiplicacao);
 
-		Divisao div = new Divisao();
-		int divisao = div.dividir(3, 7);
-		System.out.println(divisao);
-
 		// DIVISÃO
+		Divisao div = new Divisao(); //instanciando a classe Divisão com nome "div"
+		int divisao = div.dividir(3, 7); //criando um atributo do tipo int, chamando o metodo dividir com os valores declarados
+		System.out.println(divisao); //printando o atibuto "Divisão" no console, 
+
 		divisao = div.dividir(3, 0);
 		System.out.println(divisao);
 
