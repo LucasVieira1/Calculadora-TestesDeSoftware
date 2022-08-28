32f098f64911715fab8bb7e19bef2ad6c6a84bdf
LucasVieira1 <vieiraalucas1@gmail.com>
2022-08-28 16:05:07 -0300

    atualização main

--- src/Multiplicacao.java
+++ src/Multiplicacao.java
@@ -1,6 +1,6 @@
 
-public class Multiplicacao {
-	public int multiplicar(int a, int b) {
-		return a * b;
+public class Multiplicacao { // criando a classe Multiplicacao
+	public int multiplicar(int a, int b) { // criando o metodo "multiplicar", com 2 inteiros como parãmetro
+		return a * b; // o retorno vai ser a multiplicação dos 2 parãmetros
 	}
 }
--- src/Soma.java
+++ src/Soma.java
@@ -1,6 +1,6 @@
 
-public class Soma {
-	public int somar(int a, int b) {
-		return a + b;
-}
+public class Soma { // criando a classe Soma
+	public int somar(int a, int b) { // criando o metodo "somar", com 2 inteiros como parãmetro
+		return a + b; // o retorno vai ser a soma dos 2 parãmetros
+	}
 }
